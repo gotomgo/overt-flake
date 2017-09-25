54da820a11a5d14b4ecc97de9d27e99169644363
Tom Guinther <tguinther@overtone.studio>
2017-09-25 10:57:37 -0400

    stricter checks on requested size for hardward id

--- flake/macHardwareIDProvider.go
+++ flake/macHardwareIDProvider.go
@@ -6,6 +6,9 @@ import (
 	"net"
 )
 
+// macHardwareIDProvider implments HardwareIDProvider and produces a SHA1 of
+// all available MAC hardward addresses (concatenated) to create bytes for
+// use as a HardwareID
 type macHardwareIDProvider struct{}
 
 // NewMacHardwareIDProvider creates a new instance of macHardwareIDProvider
@@ -18,7 +21,7 @@ func (mac *macHardwareIDProvider) GetHardwareID(byteSize int) ([]byte, error) {
 	// On the lower bound we presume the caller(s) will do something reasonable. For the
 	// upper bound we are limited to the 20 bytes comprising the SHA1 calculated from
 	// the discovered MAC addresses
-	if byteSize > 20 {
+	if (byteSize < MACAddressLength) || (byteSize > 20) {
 		return nil, ErrInvalidSizeForHardwareAddress
 	}
 
