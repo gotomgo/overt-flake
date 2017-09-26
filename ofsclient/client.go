a6392be53369e08fa50a48576abce693147f3ac8
Tom Guinther <tguinther@overtone.studio>
2017-09-26 07:08:03 -0400

    less gross, more apis

--- ofsclient/client.go
+++ ofsclient/client.go
@@ -2,7 +2,6 @@ package ofsclient
 
 import (
 	"encoding/binary"
-	"errors"
 	"math/big"
 	"math/rand"
 	"net"
@@ -11,91 +10,171 @@ import (
 	"github.com/gotomgo/overt-flake/flake"
 )
 
-var (
-	ErrNoAddrs      = errors.New("no network addresses provided")
-	ErrInvalidToken = errors.New("auth token > 255 bytes in length")
-)
-
-type Client struct {
-	mu    sync.Mutex
-	cn    net.Conn
-	addrs []string
-	token string
+type client struct {
+	mutex     sync.Mutex
+	conn      net.Conn
+	servers   []string
+	authToken string
 }
 
-func New(token string, addrs ...string) (*Client, error) {
-	if len(addrs) == 0 {
-		return nil, ErrNoAddrs
+// NewClient creates an instance of client which implements Client
+func NewClient(authToken string, servers ...string) (Client, error) {
+	if len(servers) == 0 {
+		return nil, ErrNoServers
 	}
 
-	if len(token) > 255 {
-		return nil, ErrInvalidToken
+	if len(authToken) > 255 {
+		return nil, ErrAuthTokenTooLarge
 	}
 
-	return &Client{token: token, addrs: addrs}, nil
+	return &client{authToken: authToken, servers: servers}, nil
 }
 
-func (c *Client) connect() (err error) {
-	n := rand.Intn(len(c.addrs))
-	c.cn, err = net.Dial("tcp", c.addrs[n])
+func (c *client) Close() {
+	if c.conn != nil {
+		c.conn.Close()
+		c.conn = nil
+	}
+}
+
+// connect selects an available server endpoint and establishes a connection
+// and initiates authentication
+func (c *client) connect() (err error) {
+	// select a server
+	n := rand.Intn(len(c.servers))
+
+	// connect to server
+	c.conn, err = net.Dial("tcp", c.servers[n])
 	if err != nil {
 		return
 	}
 
-	return c.auth()
+	// authenticate (as necessary)
+	return c.authenticate()
 }
 
-func (c *Client) auth() (err error) {
-	if c.token != "" {
-		authBytes := make([]byte, 4+len(c.token))
-		binary.BigEndian.PutUint32(authBytes[0:4], uint32(0xFFFFFF00|len(c.token)))
-		copy(authBytes[4:], []byte(c.token))
-		_, err = c.cn.Write(authBytes)
+// authenticate writes an authentication header and auth token to the server IFF
+// len(client.authToken) > 0
+func (c *client) authenticate() (err error) {
+	// do we need to auth?
+	if len(c.authToken) > 0 {
+		// create the header 0xFFFFFFnn where nn is the length of the auth token
+		authBytes := make([]byte, 4)
+		binary.BigEndian.PutUint32(authBytes[0:4], uint32(0xFFFFFF00|len(c.authToken)))
+
+		// write the auth header
+		_, err = c.conn.Write(authBytes)
+		if err != nil {
+			return
+		}
+
+		// write the auth token
+		_, err = c.conn.Write([]byte(c.authToken))
+		// @NOTE: We should probably assume the token was bad and return ErrAuthFailed
+
 		return
 	}
 	return
 }
 
-func (c *Client) Gen(n int) (bigInts []big.Int, err error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+// GenerateIDBytes generates count number of overt-flake identifiers in raw/byte form
+func (c *client) GenerateIDBytes(count int) (ids []byte, err error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 
+	// on exit, if there was an error, clear the connection
 	defer func() {
 		if err != nil {
-			c.cn = nil
+			c.conn = nil
 		}
 	}()
 
-	if c.cn == nil {
+	// if we don't have a connection establish one
+	if c.conn == nil {
 		err = c.connect()
 		if err != nil {
 			return
 		}
 	}
 
+	// create the command header, which is the count of ids
 	countBytes := make([]byte, 4)
-	binary.BigEndian.PutUint32(countBytes, uint32(n))
+	binary.BigEndian.PutUint32(countBytes, uint32(count))
 
-	_, err = c.cn.Write(countBytes)
+	// write the count
+	_, err = c.conn.Write(countBytes)
 	if err != nil {
 		return
 	}
 
-	bigInts = make([]big.Int, n)
+	// the number of bytes we expect to read
+	expectedBytes := count * flake.OvertFlakeIDLength
 
-	ids := make([]byte, flake.OvertFlakeIDLength*n)
-	err = binary.Read(c.cn, binary.BigEndian, ids)
+	// create a bytes buffer to accumulate the ids
+	ids = make([]byte, expectedBytes)
+	// read the ids
+	bytesRead, err := c.conn.Read(ids)
+	if err != nil {
+		return
+	}
 
-	for i := 0; i < n; i++ {
+	// did we read all the byes we expected?
+	if bytesRead != expectedBytes {
+		return nil, ErrShortRead
+	}
+
+	return
+}
+
+// GenerateIDs generates count overt-flake identifiers in the form of []big.Int
+func (c *client) GenerateIDsAsBigInt(count int) (bigInts []big.Int, err error) {
+	ids, err := c.GenerateIDBytes(count)
+	if err != nil {
+		return
+	}
+
+	bigInts = make([]big.Int, count)
+
+	// convert each 16 bytes (flake.OvertFlakeIDLength) into a big.Int
+	for i := 0; i < count; i++ {
 		offset := i * flake.OvertFlakeIDLength
 		flakeID := flake.NewOvertFlakeID(ids[offset : offset+flake.OvertFlakeIDLength])
 		bigInts[i] = *flakeID.ToBigInt()
 	}
+
 	return
 }
 
-func (c *Client) GenOne() (*big.Int, error) {
-	ids, err := c.Gen(1)
+// GenerateID generates a single overt-flake identifier in the form of a *big.Int
+func (c *client) GenerateIDAsBigInt() (*big.Int, error) {
+	ids, err := c.GenerateIDsAsBigInt(1)
+	if len(ids) == 0 {
+		return nil, err
+	}
+	return &ids[0], nil
+}
+
+// GenerateIDsAsFlake generates count overt-flake identifiers in the form of []flake.OvertFlakeID
+func (c *client) GenerateIDsAsFlake(count int) (flakes []flake.OvertFlakeID, err error) {
+	ids, err := c.GenerateIDBytes(count)
+	if err != nil {
+		return
+	}
+
+	flakes = make([]flake.OvertFlakeID, count)
+
+	// convert each 16 bytes (flake.OvertFlakeIDLength) into a Flake
+	for i := 0; i < count; i++ {
+		offset := i * flake.OvertFlakeIDLength
+		flakes[i] = flake.NewOvertFlakeID(ids[offset : offset+flake.OvertFlakeIDLength])
+	}
+
+	return
+}
+
+// GenerateID generates a single overt-flake identifier in the form of a *big.Int
+func (c *client) GenerateIDAsFlake() (*flake.OvertFlakeID, error) {
+	ids, err := c.GenerateIDsAsFlake(1)
 	if len(ids) == 0 {
 		return nil, err
 	}
