a7c9e2687b4698082678744c21d6889a8406996c
Tom Guinther <tguinther@overtone.studio>
2017-09-26 10:14:52 -0400

    added serverConfig support and updated server console to load and use configuration two sample configs

--- main.go
+++ main.go
@@ -21,10 +21,15 @@ Options:
     -epoch           specify the epoch in milliseconds elapsed since Unix Epoch         default=1483228800000
     -waitfor         specify a time at which id generation may start, but not before    default=0
     -auth            specify the sequence of characters that make up the auth token     default=""
+    -config          specify a path to a configuration file                             default=""
+
+Notes:
+* arguments specified on the command-line override values specified in -config file
+* waitfor *must* be specified on the command line
 
 Hid Types:
     simple           simple MAC hardware ID provider
-    mac              standard MAC hardward ID provider (default)
+    mac              standard MAC hardware ID provider (default)
 
 Generator Types:
     default          the standard overt-flake ID generator
@@ -41,7 +46,7 @@ func showUsage() {
 
 func showAppVersion() {
 	fmt.Fprintf(os.Stderr, `ofsrvr: %s, Runtime: %s, Compiler: %s, Copyright © 2017 Overtone Studios, Inc.`,
-		"0.2.1",
+		"0.3.1",
 		runtime.Compiler,
 		runtime.Version())
 	fmt.Fprintln(os.Stderr, "")
@@ -95,21 +100,23 @@ func main() {
 	showAppVersion()
 
 	var showVersion bool
-	var ipAddr string
+	var argIPAddr string
 	var waitForTime int64
-	var hidType string
-	var genType string
-	var epoch int64
-	var authToken string
+	var argHidType string
+	var argGenType string
+	var argEpoch int64
+	var argAuthToken string
+	var configPath string
 
-	flag.StringVar(&ipAddr, "ip", "0.0.0.0:4444", "the interface/address to listen on")
+	flag.StringVar(&argIPAddr, "ip", "", "the interface/address to listen on")
 	flag.Int64Var(&waitForTime, "waitfor", 0, "the time to wait for prior to generating ids")
-	flag.StringVar(&hidType, "hidtype", "mac", "the hardware id provider")
-	flag.StringVar(&genType, "gentype", "default", "the type of the id generator")
-	flag.StringVar(&authToken, "auth", "", "the auth token used to authenticate clients")
-	flag.Int64Var(&epoch, "epoch", flake.OvertoneEpochMs, "the epoch used for id generation")
+	flag.StringVar(&argHidType, "hidtype", "", "the hardware id provider")
+	flag.StringVar(&argGenType, "gentype", "", "the type of the id generator")
+	flag.StringVar(&argAuthToken, "auth", "", "the auth token used to authenticate clients")
+	flag.Int64Var(&argEpoch, "epoch", -1, "the epoch used for id generation")
 	flag.BoolVar(&showVersion, "version", false, "print ofsrvr version information")
 	flag.BoolVar(&showVersion, "v", false, "print ofsrvr version information")
+	flag.StringVar(&configPath, "config", "", "the path to a ofs server configuration file")
 
 	flag.Usage = showUsage
 	flag.Parse()
@@ -118,8 +125,65 @@ func main() {
 		os.Exit(0)
 	}
 
+	//	---------------------------------------------------------
+	//	Create a server configuration with default values
+	//	---------------------------------------------------------
+
+	var config = &serverConfig{
+		IPAddr:    "0.0.0.0:4444",
+		HidType:   "mac",
+		GenType:   "default",
+		Epoch:     flake.OvertoneEpochMs,
+		AuthToken: "",
+	}
+
+	//	---------------------------------------------------------
+	//	Load a server configuration from a file if appropriate
+	//	(replacing the defaults)
+	//	---------------------------------------------------------
+
+	var err error
+	if len(configPath) > 0 {
+		config, err = loadServerConfig(configPath)
+		if err != nil {
+			showError("Error loading configuration from '%s': %s", configPath, err)
+		}
+	}
+
+	//	---------------------------------------------------------
+	//	Override configuration where appropriate
+	//	---------------------------------------------------------
+
+	if len(argHidType) > 0 {
+		config.HidType = argHidType
+	}
+
+	if len(argGenType) > 0 {
+		config.GenType = argGenType
+	}
+
+	if argEpoch > -1 {
+		config.Epoch = argEpoch
+	}
+
+	if len(argIPAddr) > 0 {
+		config.IPAddr = argIPAddr
+	}
+
+	if len(argAuthToken) > 0 {
+		config.AuthToken = argAuthToken
+	}
+
+	//	---------------------------------------------------------
+	//	Create the components needed to run the server
+	//
+	//	- Hardware ID (via HardwareIDProvider)
+	//	- overt-flake ID Generator
+	//	- overt-flake ID Server
+	//	---------------------------------------------------------
+
 	// create the hardward id provider
-	hidProvider, err := createHardwareIDProvider(hidType)
+	hidProvider, err := createHardwareIDProvider(config.HidType)
 	if err != nil {
 		showError("Error creating HardwareIDProvider: %s", err)
 	}
@@ -131,13 +195,16 @@ func main() {
 	}
 
 	// create an ID generator
-	generator, err := createOvertFlakeIDGenerator(genType, epoch, hid, waitForTime)
+	generator, err := createOvertFlakeIDGenerator(config.GenType, config.Epoch, hid, waitForTime)
 	if err != nil {
 		showError("Error creating Overt-Flake generator: %s", err)
 	}
 
 	// create an OvertFlakeServer
-	server := ofsserver.NewOvertFlakeServer(generator, ipAddr, authToken)
+	server, err := ofsserver.NewOvertFlakeServer(generator, config.IPAddr, config.AuthToken)
+	if err != nil {
+		showError("Error creating Overt-Flake server: %s", err)
+	}
 
 	// Wait for SIGINT and SIGTERM (HIT CTRL-C)
 	ch := make(chan os.Signal)
@@ -148,6 +215,29 @@ func main() {
 			os.Exit(0)
 		}
 	}()
+
+	//	---------------------------------------------------------
+	//	Echo the server configuration
+	//	---------------------------------------------------------
+
+	fmt.Fprintf(os.Stderr, "Starting overt-flake ID server on %s\n", config.IPAddr)
+	fmt.Fprintf(os.Stderr, "  with epoch = %d\n", config.Epoch)
+	fmt.Fprintf(os.Stderr, "  with hardware id = %v\n", hid)
+	fmt.Fprintf(os.Stderr, "  with generator type = %s\n", config.GenType)
+
+	if waitForTime != 0 {
+		fmt.Fprintf(os.Stderr, "  with waitForTime = %d\n", waitForTime)
+	}
+
+	if len(config.AuthToken) == 0 {
+		fmt.Fprintln(os.Stderr, "  with server AUTH ****DISABLED****")
+	} else {
+		fmt.Fprintf(os.Stderr, "  with server AUTH enabled (%s)\n", config.AuthToken)
+	}
+
+	//	---------------------------------------------------------
+	//	Run the server
+	//	---------------------------------------------------------
 
 	err = server.Serve()
 	if err != nil {
---
+++ serverConfig.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"io/ioutil"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+// serverConfig is a yaml file representation of the configuration for an
+// overt-flake ID server
+type serverConfig struct {
+	IPAddr    string `yaml:"ipAddr"`
+	Epoch     int64  `yaml:"epoch"`
+	HidType   string `yaml:"hidType"`
+	GenType   string `yaml:"genType"`
+	AuthToken string `yaml:"authToken"`
+}
+
+// loadConfig loads bytes from a file and calls a function to
+// translate the bytes into an object
+func loadConfig(configPath string, f func([]byte) (interface{}, error)) (interface{}, error) {
+	configFile, err := ioutil.ReadFile(configPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	config, err := f(configFile)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+// loadServerConfig loads an overt-flake server configuration from a yaml file
+func loadServerConfig(configPath string) (*serverConfig, error) {
+	c, err := loadConfig(configPath, func(bytes []byte) (interface{}, error) {
+		var config serverConfig
+		err := yaml.Unmarshal(bytes, &config)
+		if err != nil {
+			return nil, err
+		}
+		return &config, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return c.(*serverConfig), nil
+}
+
+// saveFile saves bytes to a file with owner RW permissions, and R for group and all users
+func saveFile(path string, configBytes []byte) error {
+	return ioutil.WriteFile(path, configBytes, 0644)
+}
+
+// saveServerConfig is a helper function used to save a serverConfig
+func saveServerConfig(path string, config *serverConfig) error {
+	bytes, err := yaml.Marshal(config)
+	if err != nil {
+		return err
+	}
+
+	return saveFile(path, bytes)
+}
