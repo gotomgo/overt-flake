ac3b94b157fc5940f9fb2951395a067cd3023416
Tom Guinther <tguinther@overtone.studio>
2017-09-27 10:07:57 -0400

    added StreamIDBytes StreamFlakes, and StreamBigInts

--- ofsclient/types.go
+++ ofsclient/types.go
@@ -15,6 +15,9 @@ type Client interface {
 	// GenerateID generates a single ID in []byte form
 	GenerateID() (id []byte, err error)
 
+	// StreamIDBytes generates ids in chunks passing them back to the caller as they arrive, via buffer
+	StreamIDBytes(count int, buffer []byte, callback func(int, []byte) error) (totalAllocated int, err error)
+
 	// Close closes any open connection. If any generate call is made the connection
 	// will be re-established
 	Close()
@@ -25,10 +28,17 @@ type Client interface {
 type OvertFlakeClient interface {
 	Client
 
+	// StreamBigInts generates big.Int's in chunks passing them back to the caller as they arrive, via buffer
+	StreamBigInts(count int, bigInts []big.Int, callback func(int, []big.Int) error) (totalAllocated int, err error)
+
 	// GenerateBigInt generates a single ID as a *big.Int
 	GenerateBigInt() (*big.Int, error)
+
 	// GenerateBigInts generates count IDs in the form of []big.Int
 	GenerateBigInts(count int) (bigInts []big.Int, err error)
+
+	// StreamFlakes generates ids in chunks passing them back to the caller as they arrive, via buffer
+	StreamFlakes(count int, flakes []flake.OvertFlakeID, callback func(int, []flake.OvertFlakeID) error) (totalAllocated int, err error)
 
 	// GenerateFlakes generates count IDs in the form of []flake.OvertFlakeID
 	GenerateFlakes(count int) (flakes []flake.OvertFlakeID, err error)
