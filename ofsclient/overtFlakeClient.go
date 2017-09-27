dfa7e4024ac050c46be4ad127fb9b652d9a689ba
Tom Guinther <tguinther@overtone.studio>
2017-09-27 10:07:29 -0400

    implement StreamBigInts and StreamFlakes

--- ofsclient/overtFlakeClient.go
+++ ofsclient/overtFlakeClient.go
@@ -23,6 +23,26 @@ func NewOvertFlakeClient(authToken string, servers ...string) (OvertFlakeClient,
 	return &overtFlakeClient{client: client{authToken: authToken, servers: servers, idSize: flake.OvertFlakeIDLength}}, nil
 }
 
+// StreamFlakes generates ids in chunks passing them back to the caller as they arrive, via buffer
+func (c *overtFlakeClient) StreamBigInts(count int, bigInts []big.Int,
+	callback func(int, []big.Int) error) (totalAllocated int, err error) {
+
+	// create a byte buffer with a length that corresponds to the flakes buffer provided by caller
+	buffer := make([]byte, len(bigInts)*flake.OvertFlakeIDLength)
+	totalAllocated, err = c.StreamIDBytes(count, buffer, func(count int, ids []byte) error {
+		// as the id bytes stream in, convert them to flakes
+		for i := 0; i < count; i++ {
+			index := i * flake.OvertFlakeIDLength
+			bigInts[i] = *flake.NewOvertFlakeID(buffer[index : index+flake.OvertFlakeIDLength]).ToBigInt()
+		}
+
+		// send the flakes to our caller
+		return callback(count, bigInts)
+	})
+
+	return
+}
+
 // GenerateIDs generates count overt-flake identifiers in the form of []big.Int
 func (c *overtFlakeClient) GenerateBigInts(count int) (bigInts []big.Int, err error) {
 	ids, err := c.GenerateIDBytes(count)
@@ -49,6 +69,26 @@ func (c *overtFlakeClient) GenerateBigInt() (*big.Int, error) {
 		return nil, err
 	}
 	return &ids[0], nil
+}
+
+// StreamFlakes generates ids in chunks passing them back to the caller as they arrive, via buffer
+func (c *overtFlakeClient) StreamFlakes(count int, flakes []flake.OvertFlakeID,
+	callback func(int, []flake.OvertFlakeID) error) (totalAllocated int, err error) {
+
+	// create a byte buffer with a length that corresponds to the flakes buffer provided by caller
+	buffer := make([]byte, len(flakes)*flake.OvertFlakeIDLength)
+	totalAllocated, err = c.StreamIDBytes(count, buffer, func(count int, ids []byte) error {
+		// as the id bytes stream in, convert them to flakes
+		for i := 0; i < count; i++ {
+			index := i * flake.OvertFlakeIDLength
+			flakes[i] = flake.NewOvertFlakeID(buffer[index : index+flake.OvertFlakeIDLength])
+		}
+
+		// send the flakes to our caller
+		return callback(count, flakes)
+	})
+
+	return
 }
 
 // GenerateIDsAsFlake generates count overt-flake identifiers in the form of []flake.OvertFlakeID
